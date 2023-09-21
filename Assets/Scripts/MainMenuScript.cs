b08b5a135040e5bbfed446459721ba4e8b1713b5
100bpm <143532808+100bpm@users.noreply.github.com>
2023-09-21 18:10:31 -0400

    added main menu

---
+++ Assets/Scripts/MainMenuScript.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class mainmenuscript : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public void StartGame(string sceneName) 
+    {
+        Debug.Log("start");
+        SceneManager.LoadScene(sceneName);
+    
+    }
+
+    public void Exit() 
+    {
+        Debug.Log("exit");
+    
+    }
+
+
+}
